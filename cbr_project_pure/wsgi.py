40eb55e51100e8756726617060137e355740df66
mitsuk-maksim <mitsuk.maksim@gmail.com>
2021-09-29 17:24:03 +0300

    Initial commit

---
+++ cbr_project_pure/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cbr_project_pure project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cbr_project_pure.settings')
+
+application = get_wsgi_application()
